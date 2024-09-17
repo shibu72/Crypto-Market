8f423d5cde00df4ea11dab9af53b9f88e965b112
Shawon Mondol Shibu <Shawonmondol72@gmail.com>
2024-09-17 06:00:50 +0600

    Update Navbar.jsx

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -4,7 +4,9 @@ export default function Navbar() {
   return (
     <>
       <nav className="flex flex-wrap items-center justify-between fixed top-0 left-0 w-full m-auto bg-base-200 fixed z-10">
+        <Link to={'/'}>
         <img className="w-16 h-16" src="/btcCoin.png" alt="logo" />
+        </Link>
 
         <div>
           <ul className="list-none flex flex-wrap gap-5 capitalize font-semibold">
